94421c8fb7e6cc37936bb26423c58ea796514c37
HomeroValdovinos <31164951+HomeroValdovinos@users.noreply.github.com>
2017-09-20 18:35:13 -0500

    Update uno.py
    This test is succesfull

--- uno.py
+++ uno.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
 import time
 import webbrowser
 
 def sleeping():
-    time.sleep(2.5)
+    time.sleep(10)
 
 def openBrowser():
-   url="http://iwmstoday.webfarm.ms.com/mstoday/"
+   url="https://www.youtube.com/watch?v=ZgewiFcvChw"
    webbrowser.open_new(url)
+   print "The url test has been successful"
 
 sleeping()
 openBrowser()

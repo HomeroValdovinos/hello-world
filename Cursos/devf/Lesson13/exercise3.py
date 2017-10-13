50fd4d4a0788e0fac2ebf6e027be9ff65e474dd1
HomeroValdovinos <31164951+HomeroValdovinos@users.noreply.github.com>
2017-10-13 15:03:04 -0500

    Update exercise3.py

--- Cursos/devf/Lesson13/exercise3.py
+++ Cursos/devf/Lesson13/exercise3.py
@@ -1,16 +1,50 @@
-# Program que calcular la longitud de una cadena
+# Program que calcular la longitud de una string_new
+
+
+class Main(object):
+    def __init__(self):
+        running = 1
+        string_new = []
+        while running == 1:
+            string_new = str(input('Enter <Carriage return> only to exit: '))
+            if string_new == '':
+                running = 0
+            else:
+                Calculate(string_new)
+                break
 
 
 class Calculate(object):
-    def __init__(self, list):
-        self.list = list
+    def __init__(self, list_new):
+        self.list_new = list_new
         self.count = 0
-        for x in range(len(self.list)):
+        for x in range(len(self.list_new)):
             self.count = x + 1
         print("The size of the string is", self.count)
 
 
-list_1 = ['h', 1, 20, 'homero', 345]
+Main()
 
-Calculate(list_1)
 
+# a = []
+# prompt = "Type the string: \n"
+# line = input(prompt)
+#
+# while line:
+#     a.append(str(line))
+#     line = input(prompt)
+# print(a)
+#
+#
+# running = 1
+# string_new = []
+# while running == 1:
+#     string_new = str(input('Enter <Carriage return> only to exit: '))
+#     if string_new == '':
+#         running = 0
+#     else:
+#         break
+#
+# print(string_new)
+# print(len(string_new))
+#
